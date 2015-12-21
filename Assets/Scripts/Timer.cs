e68d3048d9e32b6e09e3a1d388f2efbc4fc0d1b4
Kim Wong <kimspindel@gmail.com>
2015-12-21 23:26:48 +0100

    added particle effects to the timer slider and made the beat occur halfway through the timer slider

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -8,20 +8,34 @@ public class Timer : MonoBehaviour {
     Slider slider;
 
     public MusicBox musicBox;
+    public float beatsPerMinute;
+    float beatLength;
+    float halfBeat;
+    float sliderValue;
 
-    public float beatLength;
+    public ParticleSystem timerParticles;
 
 	void Start () {
         slider = timerSlider.GetComponent<Slider>();
-        slider.maxValue = beatLength;
+        beatLength = 60.0f / beatsPerMinute;
+        halfBeat = 30.0f / beatsPerMinute;
+        sliderValue = 0.0f;
+        slider.maxValue = 1.0f;
 	}
 	
 	void Update () {
         float currentTime = Time.time;
         float beatTime = currentTime % beatLength;
-	    slider.value = beatTime;
+	    sliderValue = beatTime + halfBeat;
+        if(sliderValue > beatLength) {
+            sliderValue = sliderValue - beatLength;
+        }
+
+        slider.value = sliderValue;
+        // on beat //
         if(Mathf.Abs(beatTime - beatLength) < 0.02) {
             musicBox.Kick();
+            timerParticles.Emit(30);
         }
 	}
 }
