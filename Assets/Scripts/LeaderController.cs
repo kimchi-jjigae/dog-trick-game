8df271c58eda760cd8d620bc6c039d43532c8891
Kim Wong <kimspindel@gmail.com>
2015-12-26 17:10:09 +0100

    created a PlayerController class that the Dog and LeaderController inherit from

--- Assets/Scripts/LeaderController.cs
+++ Assets/Scripts/LeaderController.cs
@@ -1,35 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LeaderController : MonoBehaviour {
+public class LeaderController : PlayerController {
 
-    public LevelPlayer level;
-    public DogState state;
-    DogSoundController soundController;
-    Animator animator;
-    float timeAtNewState;
-    float stateDuration;
-
-	void Start () {
-	    state = DogState.Idle;
-        animator = GetComponent<Animator>();
-        soundController = GetComponent<DogSoundController>();
-        stateDuration = 0.4f; // seconds
-        timeAtNewState = 0.0f;
-	}
-	
-	void Update() {
-        // is it time to idle again?! //
-        animator.SetInteger("DogState", (int)state);
-
-        if(state != DogState.Idle) {
-            if(stateTimeOver()) {
-                state = DogState.Idle;
-            }
-        }
-	}
-
-    public void OnBeat() {
+    public override void OnBeat() {
         if(level.IsLeading()) {
             state = level.CurrentDogState();
             timeAtNewState = Time.time;
@@ -37,9 +11,5 @@ public class LeaderController : MonoBehaviour {
                 soundController.Bark();
             }
         }
-    }
-
-    bool stateTimeOver() {
-        return Time.time - timeAtNewState > stateDuration;
     }
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum DogState {
+    Idle = 0,
+    Sit,
+    Bark
+}
+
+public class PlayerController : MonoBehaviour {
+
+    public LevelPlayer level;
+    public DogState state;
+    protected DogSoundController soundController;
+    Animator animator;
+    protected float timeAtNewState;
+    float stateDuration;
+
+	void Start () {
+	    state = DogState.Idle;
+        animator = GetComponent<Animator>();
+        soundController = GetComponent<DogSoundController>();
+        stateDuration = 0.4f; // seconds
+        timeAtNewState = 0.0f;
+	}
+	
+	void Update() {
+        // is it time to idle again?! //
+        animator.SetInteger("DogState", (int)state);
+
+        if(state != DogState.Idle) {
+            if(stateTimeOver()) {
+                state = DogState.Idle;
+            }
+        }
+	}
+
+    public virtual void OnBeat() {
+        if(!level.IsLeading()) {
+            //state = level.CurrentDogState();
+            timeAtNewState = Time.time;
+        }
+    }
+
+    bool stateTimeOver() {
+        return Time.time - timeAtNewState > stateDuration;
+    }
+}
